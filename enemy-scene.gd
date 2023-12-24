a3190cf2663879c6f458bba966e250bac37de1ac
bobertsanchezez <henry_earnest@brown.edu>
2023-12-23 21:37:59 -0600

    tweaked enemy behavior

--- enemy-scene.gd
+++ enemy-scene.gd
@@ -5,11 +5,9 @@ extends RigidBody2D
 func _ready():
 	$AnimatedSprite2D.play("move")
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
